3c9bfc8b1bbadd0b360d24fefc5336a2798198a9
jessejiang <jessej1020@gmail.com>
2025-12-28 15:50:34 -0500

    Updated controller interface

--- src/main/java/com/store/controller/Controller.java
+++ src/main/java/com/store/controller/Controller.java
@@ -1,3 +1,13 @@
 package com.store.controller;
 
-public interface Controller {}
+import com.store.model.Product;
+
+public interface Controller {
+    void loadProducts();
+    void addToCart(Product product);
+    void removeFromCart(Product product);
+    void updateCartQuantity(Product product, int quantity);
+    void clearCart();
+    void showProductList();
+    void showCart();
+}
