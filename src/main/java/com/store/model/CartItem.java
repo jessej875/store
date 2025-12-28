53c5d29287c6bda92a5740c793f675b7aacce776
jessejiang <jessej1020@gmail.com>
2025-12-28 14:29:07 -0500

    Made cartitem class to track a product in the cart

---
+++ src/main/java/com/store/model/CartItem.java
@@ -0,0 +1,37 @@
+package com.store.model;
+
+public class CartItem {
+    private final Product product;
+    private int quantity;
+
+    public CartItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void incrementQuantity() {
+        this.quantity++;
+    }
+
+    public void decrementQuantity() {
+        if (this.quantity > 0) {
+            this.quantity--;
+        }
+    }
+
+    public double getTotalPrice() {
+        return product.getPrice() * quantity;
+    }
+}
