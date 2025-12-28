8e2a33ac1717ea4e445221fa7ef4ed2be52c52d0
jessejiang <jessej1020@gmail.com>
2025-12-28 16:51:57 -0500

    Updated view class to be able to render the productlistview or cartview

--- src/main/java/com/store/view/View.java
+++ src/main/java/com/store/view/View.java
@@ -1,3 +1,41 @@
 package com.store.view;
 
-public class View {}
+import com.store.controller.Controller;
+import com.store.controller.ControllerImpl;
+import com.store.model.Observer;
+import com.store.model.Store;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class View implements FXComponent, Observer {
+    private final Controller controller;
+    private final Store store;
+    private final Stage stage;
+
+    public View(Controller controller, Store store, Stage stage) {
+        this.controller = controller;
+        this.store = store;
+        this.stage = stage;
+    }
+
+    @Override
+    public Parent render() {
+        String currentView = ((ControllerImpl) controller).getCurrentView();
+
+        if ("cart".equals(currentView)) {
+            CartView cartView = new CartView(controller, store);
+            return cartView.render();
+        } else {
+            ProductListView productListView = new ProductListView(controller, store);
+            return productListView.render();
+        }
+    }
+
+    @Override
+    public void update() {
+        Scene scene = new Scene(render(), 1000, 700);
+        scene.getStylesheets().add(getClass().getResource("/store.css").toExternalForm());
+        stage.setScene(scene);
+    }
+}
